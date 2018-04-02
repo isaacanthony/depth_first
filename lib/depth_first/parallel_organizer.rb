e061643320873dd20721c171befd7aefebe1d0bc
Isaac Anthony <ianthony@optoro.com>
2018-04-02 09:35:29 -0400

    Add support for parallel organizer loading dependencies.

--- lib/depth_first/parallel_organizer.rb
+++ lib/depth_first/parallel_organizer.rb
@@ -1,12 +1,29 @@
 module DepthFirst
   # Base parallel organizer class
   class ParallelOrganizer < SequentialOrganizer
+    DEPENDENCIES = [].freeze
+
+    def initialize(options)
+      super(options)
+      load_dependencies
+    end
+
     def perform
       tasks.map { |task| execute_promise(task) }
            .reduce(options) { |a, e| resolve_promise(a, e) }
     end
 
     private
+
+    # Load dependencies before concurrency to get
+    # around lazy-loading race condition.
+    def load_dependencies
+      dependencies.each { |dependency| dependency }
+    end
+
+    def dependencies
+      self.class::DEPENDENCIES
+    end
 
     def execute_promise(task)
       Concurrent::Promise.new { task.new(options).perform }.execute
